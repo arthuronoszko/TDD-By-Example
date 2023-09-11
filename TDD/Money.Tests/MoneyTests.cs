af0f72c0f01406cba44a7e8c63e4bc26810fcbda
Arthur Onoszko <arthur.onoszko@gmail.com>
2023-09-11 19:52:37 +0200

    CH16

--- TDD/Money.Tests/MoneyTests.cs
+++ TDD/Money.Tests/MoneyTests.cs
@@ -108,4 +108,16 @@ public class MoneyTests
         Money result = bank.Reduce(sum, "USD");
         Assert.Equal(Money.Dollar(15), result);
     }
+
+    [Fact]
+    public void SumTimes()
+    {
+        Expression fiveBucks = Money.Dollar(5);
+        Expression tenFrancs = Money.Franc(10);
+        Bank bank = new Bank();
+        bank.AddRate("CHF", "USD", 2);
+        Expression sum = new Sum(fiveBucks, tenFrancs).Times(2);
+        Money result = bank.Reduce(sum, "USD");
+        Assert.Equal(Money.Dollar(20), result);
+    }
 }
--- TDD/Money/Expression.cs
+++ TDD/Money/Expression.cs
@@ -4,4 +4,5 @@ public interface Expression
 {
     public Money Reduce(Bank bank, string to);
     public Expression Plus(Expression addend);
+    public Expression Times(int multiplier);
 }
